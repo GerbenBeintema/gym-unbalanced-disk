eb233323df3b4eedfbde6b42516c0cde861d5431
GerbenBeintema <izaak.beintema@gmail.com>
2021-04-13 11:43:11 +0200

    re-test

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 import setuptools
+from setuptools import setup, find_namespace_packages
+
+packages = [a for a in find_namespace_packages(where='.') if a[:6]=='deepSI']
 
 setuptools.setup(
       name='gym_unbalanced_disk',
-      version='0.0.3',
+      version='0.0.4',
       description='An OpenAI gym environment for unbalanced disk.',
       url="https://github.com/GerbenBeintema/gym-unbalanced-disk",
       author = 'Gerben Beintema',
       author_email = 'g.i.beintema@tue.nl',
       license = 'BSD 3-Clause License',
       python_requires = '>=3.6',
+      packages=packages,
       install_requires = ['gym','numpy','scipy']
       )
