4bbf621099bf6803cf30e5a32ceab318dd04da2b
Dhuige <31347265+Dhuige@users.noreply.github.com>
2023-05-22 18:45:07 +0200

    Setup to fix other dataframe

--- dataloader.py
+++ dataloader.py
@@ -3,26 +3,31 @@ from pandas import read_csv, DataFrame
 from numpy import array, concatenate
 
 class DATA():
-    def __init__(self):
+    def __init__(self, na:int=15, nb:int=15):
+        """
+        Initialize the data
+        Parameters:
+            na: number of past y values used
+            nb: number of past u values used
+        returns:
+            all in the class
+        """
+
+
         self._get_data()
 
         self.train = self._rename_df(read_csv(self.trainfilepath))
         self.testsub = self._rename_df(read_csv(self.testsubfilepath))
         self.testsim = read_csv(self.testsimfilepath)
 
-        self.Xtrain, self.Ytrain = self.make_training_data(self.train.u, self.train.th, 15, 15)
-        #self.testsub_data = self.make_training_data(self.testsub.u, self.testsub.th, 15, 15)
+        self.Xtrain, self.Ytrain = self.make_training_data(self.train.u, self.train.th, na, nb)
+        #self.testsub_data = self.make_training_data(self.testsub.u, self.testsub.th, na, nb)
 
-    def get_train(self):
-        return self.train
 
-    def get_testsub(self):
-        return self.testsub
-
-    def get_testsim(self):
-        return self.testsim    
 
     def _get_data(self):
+        """Get the data from the disc-benchmark-files folder"""
+
         DATA_DIR = path.join(getcwd(),'disc-benchmark-files\\')
     
         self.trainfilepath   = path.join(DATA_DIR,'training-data.csv')
@@ -31,14 +36,13 @@ class DATA():
     
     def _get_data_transform(self, na, nb):
         """Transforms the data to the correct format for the model f(u[k-15]-u[k-1],y[k-15]-y[k-1]) and returns X and Y data"""
-
-
         self.Xtrain, self.Ytrain = self.make_training_data(self.train)
         self.Xtestsim, self.Ytestsim = self.make_training_data(self.testsim)
-        self.Xtestsub, self.Ytestsub = self.testsub , self.testsub[' y[k-0]'].iloc[1:len(self.testsub)]
+        self.Xtestsub, self.Ytestsub = self.testsub.iloc[:, 0:-1] , self.testsub[:,-1]
 
 
     def make_training_data(self, ulist:list, ylist:list, na:int, nb:int):
+        """Training Data Function (only needs to be called for training data and testsim)"""
         Xdata = []
         Ydata = []
 
@@ -54,6 +58,10 @@ class DATA():
         return DataFrame(Xdata,columns=column_names), DataFrame(Ydata, columns=['y[k]'])
 
 
-    def _rename_df(self, df):
-        """Rename the dataframe colums to something more readable/callable"""
+    def _rename_df1(self, df):
+        """Rename the dataframe colums to something more callable"""
         return df.rename(columns={'# u':'u', ' th':'th'})
+    
+    # def _rename_df2(self, df):
+    #     """Rename the dataframe colums to something more callable"""
+    #     return df.rename(columns=['# u[k-15]':'u[k-15]', ' u[k-14]':'u[k-14]', ' u[k-13]':'u[k-13]', ' u[k-12]':'u[k-12]', ' u[k-11]':'u[k-11]', ' u[k-10]':'u[k-10]',' u[k-9]':'u[k-9]', ' u[k-8]':'u[k-8]',' u[k-7]':'u[k-7]', ' u[k-6]':'u[k-6]', ' u[k-5]':'u[k-5]', ' u[k-4]':'u[k-4]',' u[k-3]':'u[k-3]', ' u[k-2]':'u[k-2]', ' u[k-1]':'u[k-1]','y[k-15]':'y[k-15]', ' y[k-14]':'y[k-14]', ' y[k-13]':'y[k-13]',' y[k-12]':'y[k-12]',' y[k-11]':'y[k-11]', ' y[k-10]':'y[k-10]', ' y[k-9]':'y[k-9]', ' y[k-8]':'y[k-8]', ' y[k-7]':'y[k-7]',' y[k-6]':'y[k-6]', ' y[k-5]':'y[k-5]', ' y[k-4]':'y[k-4]', ' y[k-3]':'y[k-3]', ' y[k-2]':'y[k-2]', ' y[k-1]':'y[k-1]',' y[k-0]':'y[k-0]'])
