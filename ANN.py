d9ccf98d80f31ea6227bacacf2e6c42b6c2e9e88
Dhuige <31347265+Dhuige@users.noreply.github.com>
2023-05-24 11:04:32 +0200

    Add ANN 1layer Model

---
+++ ANN.py
@@ -0,0 +1,15 @@
+from torch import nn
+
+class ANN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super().__init__()
+        self.fc1 = nn.Sequential(nn.LazyLinear(5),
+                                nn.LeakyReLU(),
+                                nn.Linear(5, output_dim),
+        )
+
+        
+    def forward(self, x):
+        x = self.fc1(x)
+
+        return x
