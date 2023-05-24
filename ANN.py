0a171107ad8e79a29f413e7bc003b4479e774f1a
Dhuige <31347265+Dhuige@users.noreply.github.com>
2023-05-24 11:39:25 +0200

    Show model structure

--- ANN.py
+++ ANN.py
@@ -3,7 +3,7 @@ from torch import nn
 class ANN(nn.Module):
     def __init__(self, input_dim, output_dim):
         super().__init__()
-        self.fc1 = nn.Sequential(nn.LazyLinear(5),
+        self.fc1 = nn.Sequential(nn.Linear(30, 5),
                                 nn.LeakyReLU(),
                                 nn.Linear(5, output_dim),
         )
