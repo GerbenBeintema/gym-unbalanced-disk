006fad4f2d18af723dc1487531458acc257496dd
Dhuige <31347265+Dhuige@users.noreply.github.com>
2023-05-29 11:09:53 +0200

    Add correct storing.

--- Trainer.py
+++ Trainer.py
@@ -6,17 +6,50 @@ from torch.nn import Module, MSELoss
 from torch import tensor, float32, mean
 from torch.optim import Adam
 from pandas import DataFrame
-from os.path import join
+from os.path import join, exists
 from sys import stdout
-from os import getcwd
+from os import getcwd, makedirs
 from tqdm import tqdm
 from numpy import arange
 from numpy.random import shuffle
 import matplotlib.pyplot as plt
 
-class Trainer:
+class Model_processes():
+    """Class to store the model processes"""
+    def __init__(self, model, DIR:str):
+        self.model_name = model.name
+        self.model = model
+        self.DIR = DIR
+
+        if not exists(DIR):
+            makedirs(DIR)
+
+
+    def save_model(self, DIR:str|None=None):
+        """Save the model"""
+        if DIR is not None:
+            store_path = join(DIR, self.model_name)
+        else:
+            store_path = join(self.DIR, self.model_name)
+
+        save(self.model.state_dict(), store_path)
+
+    def load_model(self, model_name:str|None, DIR:str|None=None):
+        """Load the model"""
+        if DIR is not None:
+            store_path = join(DIR, model_name)
+        else:
+            store_path = join(self.DIR, model_name)
+            
+        self.model.load_state_dict(store_path)
+
+class Trainer(Model_processes):
     """Trainer class for training a model, works for NL and ANN models (NOE use other trainer)"""
     def __init__(self, model:Module, dl_train:DataLoader, dl_val:DataLoader, dl_test:DataLoader, DIR:str):
+        """Initialize the trainer"""
+        super().__init__(model, DIR)
+
+        # Check if GPU is available
         self.device = device("cuda" if is_available() else "cpu")
         print(f"The device that will be used in training is {get_device_name(self.device)}")
 
@@ -33,7 +66,6 @@ class Trainer:
         self.train = dl_train
         self.val = dl_val
         self.test = dl_test
-        self.DIR = DIR
 
     def train_epoch(self, dl:DataLoader):
         """Train the model for one epoch
@@ -167,26 +199,8 @@ class Trainer:
         df_train.to_csv(f'{self.DIR}\\train_{self.model_name}.csv')
         df_val.to_csv(f'{self.DIR}\\val_{self.model_name}.csv')
         # Return a dataframe that logs the training process. This can be exported to a CSV or plotted directly.
-    
-    def save_model(self, DIR:str=None):
-        """Save the model"""
-        if DIR is not None:
-            store_path = join(DIR, self.model_name)
-        else:
-            store_path = join(self.DIR, self.model_name)
 
-        save(self.model.state_dict(), store_path)
-
-    def load_model(self, model_name:str, DIR:str=None):
-        """Load the model"""
-        if DIR is not None:
-            store_path = join(DIR, model_name)
-        else:
-            store_path = join(self.DIR, model_name)
-            
-        self.model.load_state_dict(store_path)
-
-class NOE_Trainer():
+class NOE_Trainer(Model_processes):
     """NOE Trainer class, for other models use the other trainer class"""
     def __init__(self, model, Xtrain:tensor, Ytrain:tensor, Xval:tensor, Yval:tensor, Xtest:tensor=None, Ytest:tensor=None, DIR:str=None):
         """Initialize the trainer
@@ -200,6 +214,8 @@ class NOE_Trainer():
             Ytest: test output
             DIR: directory to store the model data
         """
+        super().__init__(model, DIR)
+
         self.device = 'cuda' if is_available() else 'cpu'
 
         # Set the device to use for training
@@ -220,8 +236,6 @@ class NOE_Trainer():
 
         self.Xtest = Xtest.to(self.device) if Xtest is not None else None
         self.Ytest = Ytest.to(self.device) if Ytest is not None else None
-        
-        self.DIR = DIR
 
     def fit(self, epochs:int, batch_size:int, n_burn:int, plot:bool=True):
         """Fit the model
@@ -257,24 +271,6 @@ class NOE_Trainer():
         if plot:
             self.plot_results()
     
-    def save_model(self, DIR:str|None=None):
-        """Save the model"""
-        if DIR is not None:
-            store_path = join(DIR, self.model_name)
-        else:
-            store_path = join(self.DIR, self.model_name)
-
-        save(self.model.state_dict(), store_path)
-
-    def load_model(self, model_name:str|None, DIR:str|None=None):
-        """Load the model"""
-        if DIR is not None:
-            store_path = join(DIR, model_name)
-        else:
-            store_path = join(self.DIR, model_name)
-            
-        self.model.load_state_dict(store_path)
-
     def plot_results(self):
         with no_grad():
             self.model.eval()
@@ -282,7 +278,7 @@ class NOE_Trainer():
             plt.plot(self.model(inputs=self.Xval)[0].cpu().numpy(),'--')
             plt.xlabel('k')
             plt.ylabel('y')
-            plt.xlim(0,250)
+            plt.xlim(0,100)
             plt.legend(['real','predicted'])
             plt.show()
             plt.plot(mean((self.Ytrain-self.model(inputs=self.Xtrain))**2,axis=0).cpu().numpy()**0.5) #average over the error in batch
@@ -292,5 +288,3 @@ class NOE_Trainer():
             plt.grid()
             plt.show()
             self.model.train()
-
-        
